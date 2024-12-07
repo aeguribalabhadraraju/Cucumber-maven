9c3d59db1ba1c19c8b7ef30b0d1e8c92dad61bfb
bala <aeguriraju123@gmail.com>
2024-12-08 01:36:40 +0530

    added NewClass file nd modified Git file

--- src/test/java/Stepdefinition/Git.java
+++ src/test/java/Stepdefinition/Git.java
@@ -1,1 +1,22 @@
-syste.out.println("kindle")
+
+
+package Stepdefinition;
+
+public class Git {
+
+	public static void main(String[] args) {
+          
+		
+		String hungry= "yes";
+		
+		
+		if (hungry=="yes1") {
+			
+			System.out.println("eat at 1'O clock something....");
+		}
+		else {
+			System.out.println("Do work......");
+		}
+
+	}
+}
